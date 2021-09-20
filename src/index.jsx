049b7b08cea68783a4f2eadd5eafb078b184e4d4
Lara Capila <laracapilak@gmail.com>
2021-09-20 20:11:04 -0300

    Configura Context API e implementa estados globais da aplicação

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MoviesProvider from './context/MoviesProvider';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MoviesProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MoviesProvider>,
   document.getElementById('root'),
 );
